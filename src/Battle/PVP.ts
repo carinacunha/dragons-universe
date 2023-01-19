7d1599f764f95985b4394c88434e829b7975485a
Carina Cunha <carina.cunha13@gmail.com>
2023-01-19 10:25:11 -0300

    Add PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,24 @@
-// import Fighter from '../Fighter';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
+  public fighter1: Fighter;
+  public fighter2: Fighter;
+
+  constructor(fighter1: Fighter, fighter2: Fighter) {
+    super(fighter1);
+    this.fighter1 = fighter1;
+    this.fighter2 = fighter2;
+  }
+
+  public fight(): number {
+    const lifeFighter1 = this.fighter1.lifePoints;
+    const lifeFighter2 = this.fighter2.lifePoints;
+
+    while (lifeFighter1 > 0 && lifeFighter2 > 0) {
+      this.fighter1.attack(this.fighter2);
+      this.fighter2.attack(this.fighter1);
+    } 
+    return super.fight();
+  }
 }
